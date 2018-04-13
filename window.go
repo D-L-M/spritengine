6507d621210c8fc673670c80d00d90a60fd2dcd3
David Maidment <david@dmaidment.co.uk>
2018-04-13 12:56:08 +0100

    Last tweak made drift subtraction irrelevant, so removing

--- window.go
+++ window.go
@@ -18,14 +18,6 @@ func createWindow(game *Game) {
 
 	lastPaintTimeNano := time.Now().UnixNano()
 	targetFrameAgeNano := int64(1000000000) / int64(game.TargetFrameRate)
-	previousFrameRates := []float64{}
-	maxFrameRatesToConsider := 100
-	frameRateDrift := 0.0
-
-	// Prefill the previous framerate slice
-	for i := 0; i < maxFrameRatesToConsider; i++ {
-		previousFrameRates = append(previousFrameRates, float64(game.TargetFrameRate))
-	}
 
 	// Create a new window and listen for events
 	driver.Main(func(src screen.Screen) {
@@ -49,11 +41,10 @@ func createWindow(game *Game) {
 			case paint.Event:
 
 				frameAgeNano := (time.Now().UnixNano() - lastPaintTimeNano)
-				frameRateDriftNano := int64(frameRateDrift * 1000000000)
 
 				// Throttle to the desired FPS
 				if frameAgeNano < targetFrameAgeNano {
-					time.Sleep(time.Duration(targetFrameAgeNano-frameAgeNano-frameRateDriftNano) * time.Nanosecond)
+					time.Sleep(time.Duration(targetFrameAgeNano-frameAgeNano) * time.Nanosecond)
 					frameAgeNano = targetFrameAgeNano
 				}
 
@@ -66,31 +57,13 @@ func createWindow(game *Game) {
 				frameAgeSeconds := (float64(frameAgeNano) / float64(1000000000))
 				currentFrameRate := 1 / frameAgeSeconds
 
-				// Work out the average frame rate
-				previousFrameRates = append(previousFrameRates, currentFrameRate)
-
-				if len(previousFrameRates) > int(maxFrameRatesToConsider) {
-					previousFrameRates = previousFrameRates[1:]
-				}
-
-				talliedFrameRate := 0.0
-
-				for _, individualFrameRate := range previousFrameRates {
-					talliedFrameRate += individualFrameRate
-				}
-
-				averageFrameRate := talliedFrameRate / float64(maxFrameRatesToConsider)
-
-				// Figure out by how much we're straying from the target framerate
-				frameRateDrift = float64(game.TargetFrameRate) - averageFrameRate
-
 				// Repaint the stage
 				lastPaintTimeNano = time.Now().UnixNano()
 				stage := image.NewRGBA(image.Rect(0, 0, game.Width, game.Height))
 
 				game.CurrentLevel().BeforePaint(game.CurrentLevel())
 				game.CurrentLevel().Repaint(stage)
-				game.FramePainter(stage, game.CurrentLevel(), averageFrameRate)
+				game.FramePainter(stage, game.CurrentLevel(), currentFrameRate)
 				xdraw.NearestNeighbor.Scale(buf.RGBA(), image.Rect(0, 0, game.Width*game.ScaleFactor, game.Height*game.ScaleFactor), stage, stage.Bounds(), draw.Over, nil)
 				win.Upload(image.Point{}, buf, buf.Bounds())
 				win.Publish()
